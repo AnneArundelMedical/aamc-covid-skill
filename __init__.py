325c17585cdebc53f171db690a3213d9c55a434c
Nate C-K <nculwell@gmail.com>
2020-06-05 13:41:35 -0500

    Split file.

--- __init__.py
+++ __init__.py
@@ -1,15 +1,4 @@
-from mycroft import MycroftSkill, intent_file_handler
-
-
-class AamcCovid(MycroftSkill):
-    def __init__(self):
-        MycroftSkill.__init__(self)
-
-    @intent_file_handler('covid.aamc.intent')
-    def handle_covid_aamc(self, message):
-        self.speak_dialog('covid.aamc')
-
+import aamc
 
 def create_skill():
-    return AamcCovid()
-
+    return aamc.AamcCovid()
