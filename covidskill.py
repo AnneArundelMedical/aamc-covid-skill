5c18ab2bc2a66e28c77eb7217e3f6fc58690d066
Nate C-K <nculwell@gmail.com>
2020-08-11 15:32:53 -0500

    Fix name ref.

--- covidskill.py
+++ covidskill.py
@@ -84,7 +84,7 @@ class AamcCovid(MycroftSkill):
         self.api.add_message_handler("StartProning", self.__handle_message_start_proning)
         self.api.add_message_handler("StopProning", self.__handle_message_stop_proning)
         self.messenger = None
-        self.messenger = AamcCovidMessenger(self.api)
+        self.messenger = messaging.AamcCovidMessenger(self.api)
         self.cancel_scheduled_event(INIT_MESSAGING_EVENT_NAME)
         self.log.info("Init messaging successful.")
 
