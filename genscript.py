84cf90e723f9cda33d258ac134e379e5026c4cdc
Nate C-K <nculwell@gmail.com>
2020-09-14 11:25:20 -0500

    Fix script errors and yes/no choice.

--- genscript.py
+++ genscript.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 # vim: et sts=4 sw=4
 
-import sys, os, os.path, pathlib
+import sys, os, os.path, pathlib, json
+
+PARAMS_PATH = "file_params.json"
+
+FILE_PARAM_NAMES = [
+    "EXPECTED",
+]
 
 def main():
     _, source_path = sys.argv
@@ -14,6 +20,7 @@ def generate(source_path):
     filename = None
     language = None
     content_lines = []
+    file_params = {}
     for line in source:
         line = line.rstrip()
         if in_comment:
@@ -34,8 +41,12 @@ def generate(source_path):
                     language = None
                 elif cmd == "LANG":
                     language = arg
-                elif cmd == "EXPECTED":
-                    pass # ignore for now
+                elif cmd in FILE_PARAM_NAMES:
+                    if not (filename in file_params):
+                        file_params[filename] = {}
+                    if cmd in file_params[filename]:
+                        raise Exception("Duplicate param '%s' for file '%s'." % (cmd, filename))
+                    file_params[filename][cmd] = arg
                 else:
                     raise Exception("Invalid command: " + cmd)
                 write_file(old_filename, old_language, content_lines)
@@ -43,6 +54,7 @@ def generate(source_path):
             else:
                 content_lines.append(line)
     write_file(filename, language, content_lines)
+    write_params(file_params)
 
 def write_file(filename, language, content_lines):
     if filename is None:
@@ -61,6 +73,11 @@ def write_file(filename, language, content_lines):
     with open(path, "wb") as f:
         f.write(content.encode("utf-8"))
         f.write(b'\n')
+
+def write_params(file_params):
+    with open(PARAMS_PATH, "w") as f:
+        json.dump(file_params, f, indent=1, sort_keys=True)
+        print(file=f)
 
 def mkdir(directory):
     pathlib.Path(directory).mkdir(parents=True, exist_ok=True)
