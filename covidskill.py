e88e362e59d9a58d65ff5a303148d1dd164c7c4c
Nate C-K <nculwell@gmail.com>
2020-09-16 08:40:19 -0500

    Add self ref.

--- covidskill.py
+++ covidskill.py
@@ -433,7 +433,7 @@ class AamcCovid(MycroftSkill):
             "on_timeout": action_if_timeout,
         }
         self.__schedule_event(
-            handle_choice_timeout, CHOICE_TIMEOUT_DELAY_SECS, CHOICE_TIMEOUT_EVENT_NAME,
+            self.handle_choice_timeout, CHOICE_TIMEOUT_DELAY_SECS, CHOICE_TIMEOUT_EVENT_NAME,
             data={ id: rand.randint(1, 1000000) })
 
     def __cancel_choice(self):
